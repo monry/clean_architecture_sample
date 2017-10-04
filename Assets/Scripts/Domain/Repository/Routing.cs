7762ab3035d6170f153a6b68cc852cf1778b8aea
Tetsuya Mori <monry84@gmail.com>
2017-10-04 13:51:37 +0900

    :name_badge: Rename routing repository
    * スコープをどちらにするか迷ったが、 Domain 層に居ることも踏まえると Routing かな

--- Assets/Scripts/Domain/Repository/Routing.cs
+++ Assets/Scripts/Domain/Repository/Routing.cs
@@ -4,7 +4,7 @@ using UniRx;
 
 namespace CASample.Domain.Repository {
 
-    public class Scene : Repository<Scene> {
+    public class Routing : Repository<Routing> {
 
         public IObservable<Data.Entity.Scene> Load(string sceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode) {
             return SceneDataStoreFactory.CreateInstance().LoadSceneAsObservable(sceneName, loadSceneMode);
