500614dd41bbfc26cc87506fc9a1d198a15106a7
Tetsuya Mori <monry84@gmail.com>
2017-10-04 13:55:17 +0900

    :truck: Move Router to UseCase
    * 意味合いとしては UseCase に関するモノなので、一旦 UseCase.cs に同居させる

--- Assets/Scripts/CAFU/Domain/UseCase.cs
+++ Assets/Scripts/CAFU/Domain/UseCase.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 namespace CAFU.Domain {
 
     public interface IUseCase {}
 
-    public class UseCase {
+    public interface IRouter : IUseCase {}
+
+    public class UseCase : IUseCase {
+
+    }
+
+    // TODO: ルーティング専用の Repository を作ったらそっちに逃がす
+    public abstract class Router<TRepository> : IRouter where TRepository : IRepository {
+
+        public TRepository RepositoryRouting { get; set; }
 
     }
 
